056bf86d0a53657e341d5390fc29a2ae2681777d
Sebastien Windal <sebastien@windal.net>
2014-03-15 00:28:04 -0400

    Initial Commit

---
+++ SebbiToolBelt/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SebbiToolBelt
+//
+//  Created by Sebastien Windal on 3/15/14.
+//  Copyright (c) 2014 Sebastien Windal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

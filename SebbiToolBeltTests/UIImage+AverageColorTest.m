f4eb2205f0769c5c5f5ebdecf969c089f234417a
Sebastien Windal <sebastien@windal.net>
2014-03-18 23:16:48 -0400

    fixed broken tc.

--- SebbiToolBeltTests/UIImage+AverageColorTest.m
+++ SebbiToolBeltTests/UIImage+AverageColorTest.m
@@ -121,7 +121,7 @@
     {   // Note, expectedColor is the average color computed with photoshop.
         // Allow a 1% difference from it...
         UIColor *avgColor = [[UIImage imageNamed:@"BlackAndWhiteSmall.png"] stb_averageColor];
-        UIColor *expectedColor = [UIColor colorWithWhite:0.42 alpha:1.0f];
+        UIColor *expectedColor = [UIColor colorWithRed:0.42 green:0.42 blue:0.42 alpha:1.0];
         XCTAssertTrue([self compareColor:avgColor withColor:expectedColor withTolerance:0.01], @"BlackAndWhiteSmall.png");
     }
     

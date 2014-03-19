b89d6ca57fc434101206f3bfe44c0d094625e0a6
Sebastien Windal <sebastien@windal.net>
2014-03-18 23:19:48 -0400

    more debug info

--- SebbiToolBeltTests/UIImage+AverageColorTest.m
+++ SebbiToolBeltTests/UIImage+AverageColorTest.m
@@ -122,7 +122,7 @@
         // Allow a 1% difference from it...
         UIColor *avgColor = [[UIImage imageNamed:@"BlackAndWhiteSmall.png"] stb_averageColor];
         UIColor *expectedColor = [UIColor colorWithRed:0.42 green:0.42 blue:0.42 alpha:1.0];
-        XCTAssertTrue([self compareColor:avgColor withColor:expectedColor withTolerance:0.01], @"BlackAndWhiteSmall.png");
+        XCTAssertTrue([self compareColor:avgColor withColor:expectedColor withTolerance:0.01], @"BlackAndWhiteSmall.png avgColor:%@ expectedColor:%@", avgColor, expectedColor);
     }
     
 }
